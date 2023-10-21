fcd270cd08b3530a8614f026fcdcf98bb8e85311
darioaladuz <baladuz98@gmail.com>
2023-10-21 13:37:04 +0200

    Modal tabs for details & episodes

--- src/app/components/CharacterModal.js
+++ src/app/components/CharacterModal.js
@@ -1,8 +1,39 @@
 import Image from 'next/image';
 import Episodes from './Episodes';
+import { useState } from 'react';
 
 export default function CharacterModal({ closeModal, character }) {
-	console.log({ character });
+	const [currentTab, setCurrentTab] = useState('episodes');
+
+	{
+		/* Tabs for the modal */
+	}
+	const tabs = {
+		details: (
+			<>
+				<p>Name: {character.name}</p>
+				<p>Species: {character.species}</p>
+				<p>Location: {character.location.name}</p>
+				<p>Gender: {character.gender}</p>
+				<p>Status: {character.status}</p>
+			</>
+		),
+		episodes: <Episodes episodes={character.episode} />
+	};
+
+	const NavBtn = ({ title }) => {
+		return (
+			<button
+				onClick={() => setCurrentTab(title)}
+				className={`character-modal-nav-btn${
+					title === currentTab ? ' active' : ''
+				}`}
+			>
+				{title}
+			</button>
+		);
+	};
+
 	return (
 		<div className='character-modal'>
 			<div className='character-modal-header'>
@@ -20,13 +51,14 @@ export default function CharacterModal({ closeModal, character }) {
 					alt={character.name}
 				/>
 			</div>
+			<div className='character-modal-nav'>
+				{/* Dynamically get nav buttons from the 'tabs' object */}
+				{Object.keys(tabs).map((navBtn) => {
+					return <NavBtn key={navBtn} title={navBtn} />;
+				})}
+			</div>
 			<div className='character-modal-details'>
-				<p>Name: {character.name}</p>
-				<p>Species: {character.species}</p>
-				<p>Location: {character.location.name}</p>
-				<p>Gender: {character.gender}</p>
-				<p>Status: {character.status}</p>
-				<Episodes episodes={character.episode} />
+				<div className='character-modal-tab'>{tabs[currentTab]}</div>
 			</div>
 		</div>
 	);
--- src/app/components/Episode.js
+++ src/app/components/Episode.js
@@ -1,9 +1,13 @@
 export default function Episode({ episode }) {
 	return (
 		<div className='episode'>
-			<p className='episode-name'>{episode.name}</p>
-			<p className='episode-number'>{episode.episode}</p>
-			<p className='episode-date'>{episode.air_date}</p>
+			<p className='episode-name'>
+				<strong>{episode.name}</strong>
+			</p>
+			<div className='episode-footer'>
+				<p className='episode-number'>{episode.episode}</p>
+				<p className='episode-date'>{episode.air_date}</p>
+			</div>
 		</div>
 	);
 }
